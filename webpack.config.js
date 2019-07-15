a20bf620917fb5be1307989598f6c74efd39ee75
Madan <madan712@gmail.com>
2019-07-15 21:21:55 +0100

    Updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,17 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                loader: 'babel-loader'
-            }
-        ]
-    },
-    plugins: [new HtmlWebpackPlugin({
-        template: './src/index.html'
-    })]
+	mode : 'development',
+	resolve : {
+		extensions : [ '.js', '.jsx' ]
+	},
+	module : {
+		rules : [ {
+			test : /\.jsx?$/,
+			use : 'babel-loader'
+		} ]
+	},
+	plugins : [ new HtmlWebpackPlugin({
+		template : './src/index.html'
+	}) ]
 }
